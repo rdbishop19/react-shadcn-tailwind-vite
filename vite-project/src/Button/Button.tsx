bfc24dba9d827b28f48ed234a0b316f54b6a9303
Ryan Bishop <ryanb@auctionedge.com>
2024-11-13 16:36:30 -0600

    First shadcn component

---
+++ vite-project/src/Button/Button.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button"
+ 
+export default function MyButton() {
+  return (
+    <div>
+      <Button variant={"outline"}>Click me</Button>
+    </div>
+  )
+}
